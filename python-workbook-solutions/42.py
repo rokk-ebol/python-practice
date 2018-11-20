ad1a8e3724d860c4c22f0943d29b75a0a8070052
rokk-ebol <oleg.koorza@gmail.com>
2018-11-20 15:05:24 +0200

    added one file

---
+++ python-workbook-solutions/42.py
@@ -0,0 +1,26 @@
+# freq to note
+
+def freq_to_note():
+
+    C4 = 261.63
+    D4 = 293.66
+    # E4 = 329.63
+    # F4 = 349.24
+    # G4 = 392.00
+    # A4 = 440.00
+    # B4 = 493.88  
+
+    step = 1.00
+
+    freq = float(input('Enter frequency: '))
+
+    if freq >= C4 - step and freq <= C4 + step:
+        print('C4 note')
+    elif freq >= D4 - step and freq <= D4 + step:
+        print('D4 note')
+
+    '''
+    overall logic is working so i proceed to a next task
+    '''
+
+freq_to_note()
