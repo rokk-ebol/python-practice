84a50da66d8896338f5e400c79aa2634adce89e9
rokk-ebol <oleh.web@pm.me>
2018-11-24 20:29:07 +0200

    added small 'get-hasthags.py' program. input words - get hashtags.

--- automate-the-boring-stuff/get-hashtags.py
+++ automate-the-boring-stuff/get-hashtags.py
@@ -1,6 +1,5 @@
 # enter bunch of words, get hashtags with those words
 
-
 def get_hashtags():
 
     words = input('Введи слова через пробел, чтобы получить хештеги. \nЖми Enter, чтобы закончить ввод и получить результат.\t > ')
